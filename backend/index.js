5c078d8065b13434d5aae1b2e9ff277a7e113f71
Rishabh Bijalwan <rishabhbijalwan19@gmail.com>
2025-06-24 11:11:49 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
